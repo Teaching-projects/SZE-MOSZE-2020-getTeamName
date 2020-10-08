ee984f6781c1d7a80cf1d7d110e4a281fd4a4b2d
kristofelo <43994423+kristofelo@users.noreply.github.com>
2020-10-08 21:07:06 +0200

    fix github actions

--- creature.cpp
+++ creature.cpp
@@ -65,27 +65,30 @@ Creature* Creature::parseUnit(const std::string filename) {
       } else {
         std::string name, newline;
           std::vector<std::string> result;
-          int counter = 1;
           while (std::getline(file, newline)) {
+              newline = newline.substr(0, newline.length()-1);
               if (newline != "{" && newline != "}") {
                   int split = newline.find(":");
                   std::string temp = newline.substr(2,split - 2 );
-                  temp = newline.substr(split + 2);
 
-                  if(counter <= 3){
-                      temp = temp.substr(0,temp.length()-2);
-                  }else{
-                      counter=1;
-                      temp = temp.substr(0,temp.length());
-                  }
-                  if (newline.find(temp)!= std::string::npos){
+                  if (temp.find('"')!= std::string::npos)
+                  {
                       ReplaceAll2(temp, '"', "");
                   }
                   result.push_back(temp);
-                  counter++;
+                  temp = newline.substr(split + 2);
+
+                  if (temp.find(',')!= std::string::npos){
+                      ReplaceAll2(temp, ',', "");
+                  }
+                  if (temp.find('"')!= std::string::npos){
+                      ReplaceAll2(temp, '"', "");
+                  }
+                  result.push_back(temp);
               }
+
           }
-          return new Creature(result[1], std::stod(result[2]), std::stod(result[3]), 0, 1);
+          return new Creature(result[1], std::stod(result[3]), std::stod(result[5]), 0, 1);
       }
     }
 
