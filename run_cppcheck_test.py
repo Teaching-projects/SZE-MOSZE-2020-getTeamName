2b22ef2ae87c53bf6f0cb72e43e5bfe182d1c94a
kristofelo <43994423+kristofelo@users.noreply.github.com>
2020-10-10 20:57:33 +0200

    small typo fix in py script

--- run_cppcheck_test.py
+++ run_cppcheck_test.py
@@ -1,7 +1,7 @@
 import sys
 
 txt = open("artifact_cppcheck.txt","r")
-txt_content = fd.read()
+txt_content = txt.read()
 if(("error" in txt_content) or ("warning" in txt_content)):
     sys.exit(1)
 else:
