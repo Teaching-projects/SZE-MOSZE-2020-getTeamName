2c456473362b1b280cfd822ac871e21ccf0faf57
AlmasiSzabolcs <44002212+AlmasiSzabolcs@users.noreply.github.com>
2020-11-23 21:41:48 +0100

    Fix typo

--- run_cppcheck_test.py
+++ run_cppcheck_test.py
@@ -2,7 +2,7 @@ import sys
 
 txt = open("artifact_cppcheck.txt","r")
 txt_content = txt.read()
-if(("error" in txt_content) or ("warning" in txt_content)):
+if(("error:" in txt_content) or ("warning:" in txt_content)):
     sys.exit(1)
 else:
     sys.exit(0)
